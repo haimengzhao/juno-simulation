9e579f48c7e17381f8cc13afc6b8a78083238262
Haimeng Zhao <haimengzhao@icloud.com>
2021-08-28 15:46:25 +0800

    testdata generator; vertice density; petime hist

--- draw.py
+++ draw.py
@@ -3,6 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 
+# constants
+Ri = 17.71e3 # inner radius / mm
+Ro = 19.5e3 # outer radius / mm
+N_vertices = 4000 # total num of vertices
+Volume_i = 4 / 3 * np.pi * Ri ** 3 # volume of LS
+rho0 = N_vertices / Volume_i # average density / mm^-3
 
 # 该类在测试时会用到，请不要私自修改函数签名，后果自负
 class Drawer:
@@ -12,10 +18,60 @@ class Drawer:
         self.geo = geo["Geometry"]
 
     def draw_vertices_density(self, fig, ax):
-        print("TODO: Draw vertices density by radius")
+        '''
+        draw density of vertices as a function of radius:
+        density = density(radius)
+        '''
+        x = np.array(self.simtruth['x'])
+        y = np.array(self.simtruth['y'])
+        z = np.array(self.simtruth['z'])
+
+        # radius
+        r = np.sqrt(x ** 2 + y ** 2 + z ** 2)
+        # total number
+        N = len(x)
+
+        # extract hist statistics
+        # density=True: n = dN/(N * dr)
+        n, bins, patches = ax.hist(r, bins=1000, density=True)
+        ax.cla()
+
+        # plot
+        ax.set_title(r'Volume Density of Vertices $\rho(r)$')
+        ax.set_xlabel(r'Radius from Origin $r$ / $R_{LS}$')
+        ax.set_xlim(0, Ri)
+        ax.set_xticks(np.linspace(0, Ri, 11))
+        ax.set_xticklabels(['%.1f' % (i / 10) for i in range(11)])
+
+        ax.set_ylabel(r'Volume Density of Vertices $\rho(r)$ / $\rho_0 = {:.2f} \times 10^{{{:.0f}}} mm^{{-3}}$'.format(*map(float, ('%.2e'%rho0).split('e'))))
+        # ax.set_ylim(0, 2 * rho0)
+        # ax.set_yticks(np.linspace(0, 2 * rho0, 6))
+        # ax.set_yticklabels(['%.1f' % (2 * i / 5) for i in range(6)])
+        
+        # density = dN / (4 pi r^2 dr) = n / (4 pi r^2) * N
+        ax.plot(bins[1:], n / (4 * np.pi * bins[1:] ** 2) * N / rho0)
+
+        
 
     def draw_pe_hit_time(self, fig, ax):
-        print("TODO: Draw histogram of PE hit time")
+        '''
+        draw histogram of PE hit time:
+        #PE = #PE(time)
+        '''
+        time = np.array(self.petruth['PETime'])
+        maxtime = np.max(time)
+
+        # plot
+        ax.set_title(r'Histogram of PE Hit Time')
+        ax.set_xlabel(r'Hit Time $t$ / $ns$')
+        ax.set_xlim(0, maxtime)
+        ax.set_xticks(np.linspace(0, maxtime, 11))
+        ax.set_xticklabels(['%.0f' % i for i in np.linspace(0, maxtime, 11)])
+
+        ax.set_ylabel(r'Number of PE Hit')
+
+        ax.hist(time, bins=1000, density=False)
+        
 
     def draw_probe(self, fig, ax):
         print("TODO: Draw probe R(r, theta)")
---
+++ testdata.py
@@ -0,0 +1,30 @@
+import numpy as np
+import h5py
+
+# constants
+Ri = 17.71e3 # inner radius / mm
+Ro = 19.5e3 # outer radius / mm
+
+# testdata generator
+if __name__ == '__main__':
+    N = 4000
+    N_photon_per_vertex = 10000
+
+    # x = np.random.normal(loc=Ri/2, scale=Ri/8, size=N)
+    x = np.random.uniform(0, Ri, N)
+    y = x
+    z = x
+
+    PET_ChannelID = np.zeros(N * N_photon_per_vertex)
+    PETime = np.random.uniform(100, 500, N * N_photon_per_vertex)
+    with h5py.File('./data.h5', mode='w') as file:
+        file.create_group('ParticleTruth')
+        file.create_group('PETruth')
+        file['ParticleTruth']['EventID'] = np.array(range(0, N))
+        file['ParticleTruth']['x'] = x
+        file['ParticleTruth']['y'] = y
+        file['ParticleTruth']['z'] = z
+
+        file['PETruth']['EventID'] = np.array(range(0, N * N_photon_per_vertex))
+        file['PETruth']['ChannelID'] = PET_ChannelID
+        file['PETruth']['PETime'] = PETime
